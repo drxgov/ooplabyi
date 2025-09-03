17da5fedbe6c4a929a90e00cfc55d8d6d553e1fb
drxgov <retriveserenity@gmail.com>
2025-09-03 17:44:43 +0300

    up

--- 5_laba.cpp
+++ 5_laba.cpp
@@ -1,7 +1,29 @@
+/*Создать класс машина, содержащий в качестве параметров марку (строка), число цилиндров, мощность. 
+Определить конструкторы, деструктор и функцию печати. Создать public-производный класс грузовик, имеющий 
+дoполнительный вещественный параметр - грузоподъемность кузова.
+Определить конструкторы по умолчанию и с разным числом параметров, деструктор, функцию печати. 
+Определить функции переназначения мачки и грузоподъемности.*/
+
 #include <iostream>
+#include <string>
+#include <cstdio>
+
 using namespace std;
+
+class car{
+    public:
+    string brand;
+    int countOfСylinders;
+    int power;
+    car(string x, int y, int z) : brand(x), countOfСylinders(y),power(z){}
+    ~car(){};
+    void print() {
+        printf("%s %d %d",brand,countOfСylinders,power);
+    }
+};
+
+
 int main(){
-    cout << "hello world" << endl;
-    cout << "ну привет, как твои дела";
+
     return 0;
 }
