fce6b43aa9bcf8bb3891f4fdf7f164c184500393
drxgov <retriveserenity@gmail.com>
2025-09-03 18:17:57 +0300

    ya dodelal 5 labyby

--- 5_laba.cpp
+++ 5_laba.cpp
@@ -27,7 +27,7 @@ class car{
 class track:public car{
     public:
     float bodyLoadСapacity;
-    track(string x, int y , int z , int w) : car(x,y,z), bodyLoadСapacity(w){}
+    track(string x, int y , int z , float w) : car(x,y,z), bodyLoadСapacity(w){}
     ~track(){};
     void print() {
         printf("%s %d %d %.2f\n",brand.c_str(),countOfСylinders,power,bodyLoadСapacity);
@@ -41,6 +41,21 @@ class track:public car{
 };
 
 int main() {
+    car c("Toyota", 4, 150);
+    cout << "Test 1 - car print: ";
+    c.print();  // Ожидаемый вывод: Toyota 4 150
+    track t("Volvo", 6, 400, 12.5);
+    cout << "Test 2 - track print: ";
+    t.print();  // Ожидаемый вывод: Volvo 6 400 12.50
+    t.changeBrand("Scania");
+    cout << "Test 3 - change brand: ";
+    t.print();  // Ожидаемый вывод: Scania 6 400 12.50
+    t.changeBodyLoadСapacity(15.75);
+    cout << "Test 4 - change body load: ";
+    t.print();  // Ожидаемый вывод: Scania 6 400 15.75
+    car* cptr = &t;
+    cout << "Test 5 - base pointer to track: ";
+    cptr->print();  // Тут будет вызван print базового класса, так как функция не virtual
 
     return 0;
 }
